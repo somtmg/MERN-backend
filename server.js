545b32b53af35dd0595f17164736d4fcca495f95
somtmg <sombdrtmg1@gmail.com>
2025-08-26 15:52:32 +0545

    cors added

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./connect/database');
-const dotenv = require('dotenv').config();
+const express = require("express")
+const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./connect/database")
+const cors = require("cors")
+const dotenv = require("dotenv").config()
 
-const port = process.env.PORT || 5000;
-const app = express();
-
-connectDB();
+const port = process.env.PORT || 5000
+const app = express()
+// CORS for Netlify frontend
+app.use(
+  cors({
+    origin: "taupe-cascaron-f1afa3.netlify.app", // Replace with your Netlify URL
+  })
+)
+app.use(express.json())
+connectDB()
 
 // using JSON(middleware) NOTE: middleware always comes befor routes
-app.use(express.json());
-app.use(express.urlencoded(({ extended: false })))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // connecting to the routes
-app.use('/api/tasks', require('./routes/taskRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/tasks", require("./routes/taskRoutes"))
+app.use("/api/users", require("./routes/userRoutes"))
 
 // using errorHandler defined in middleware
-app.use(errorHandler);
+app.use(errorHandler)
 
 // connecting to the port
 app.listen(port, () => console.log(`Server listening on ${port}`))
